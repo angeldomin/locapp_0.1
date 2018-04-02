c408e3fcc403ad54f2588243312203600020deb6
advarea <advarea@vectoritcgroup.com>
2018-04-02 10:53:32 +0200

    Nueva pagina buscador

--- locapp/src/app/app.component.ts
+++ locapp/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { BuscadorPage } from '../pages/buscador/buscador';
 
 @Component({
   templateUrl: 'app.html'
@@ -18,11 +19,11 @@ export class MyApp {
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
+    
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Inicio', component: HomePage },
+      { title: 'Dispositivos', component: ListPage },
+      { title: 'Buscador', component: BuscadorPage }
     ];
 
   }
